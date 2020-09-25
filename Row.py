c7c8977f808645d1246bb74963cda1ae4552ddae
pythonpleb <70825351+pythonpleb@users.noreply.github.com>
2020-09-24 21:00:13 -0400

    Update Row.py

--- Row.py
+++ Row.py
@@ -2,6 +2,12 @@ class Row:
 
     def __init__(self, entries):
         self.entries = entries
+        self.point = []
+        self.bs = []
+        self.hi = []
+        self.fs = []
+        self.ifs = []
+        self.desc = []
 
     def delete(self):
         for entry in self.entries:
